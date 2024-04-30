59c30ba53e41ffc2e1fb661028bd2974a560b33b
Huy-DNA <huydo862003@gmail.com>
2024-04-30 18:16:50 +0700

    feat: better keybinding for navigating between panels

--- neovim/.config/nvim/lua/core/keymaps.lua
+++ neovim/.config/nvim/lua/core/keymaps.lua
@@ -13,3 +13,9 @@ vim.opt.shiftround = true
 vim.opt.expandtab = true
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<c-j>', '<c-w>j')
+vim.keymap.set('n', '<c-k>', '<c-w>k')
+vim.keymap.set('n', '<c-h>', '<c-w>h')
+vim.keymap.set('n', '<c-l>', '<c-w>l')
+vim.keymap.set('n', '<c-s>v', '<c-w>v')
+vim.keymap.set('n', '<c-s>h', '<c-w>s')
