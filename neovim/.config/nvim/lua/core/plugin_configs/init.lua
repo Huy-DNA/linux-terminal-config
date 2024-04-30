170b51895a16d659346d7e088ae2991b513010f9
Huy-DNA <huydo862003@gmail.com>
2024-04-30 18:06:05 +0700

    feat: add telescope and treesitter

---
+++ neovim/.config/nvim/lua/core/plugin_configs/init.lua
@@ -0,0 +1,5 @@
+require("core.plugin_configs.gruvbox")
+require("core.plugin_configs.lualine")
+require("core.plugin_configs.nvim-tree")
+require("core.plugin_configs.telescope")
+require("core.plugin_configs.treesitter")
