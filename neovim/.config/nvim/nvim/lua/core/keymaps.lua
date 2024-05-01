909cb28d3fcdee3aa63f8ccb97345d5fdcf9084e
Huy-DNA <huydo862003@gmail.com>
2024-05-01 20:28:39 +0700

    feat: make lsp work in insert mode also

---
+++ neovim/.config/nvim/nvim/lua/core/keymaps.lua
@@ -0,0 +1,22 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.api.nvim_set_keymap("n", "<leader>n", ":ASToggle<CR>", {})
+vim.keymap.set('n', '<c-j>', '<c-w>j')
+vim.keymap.set('n', '<c-k>', '<c-w>k')
+vim.keymap.set('n', '<c-h>', '<c-w>h')
+vim.keymap.set('n', '<c-l>', '<c-w>l')
+vim.keymap.set('n', '<c-s>v', '<c-w>v')
+vim.keymap.set('n', '<c-s>h', '<c-w>s')
